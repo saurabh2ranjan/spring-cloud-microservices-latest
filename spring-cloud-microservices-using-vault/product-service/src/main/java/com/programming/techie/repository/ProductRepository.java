e0df962d314bc7cf673155e0b3d70b87a413e9ac
Prateek <prateek.ashtikar512@gmail.com>
2021-04-13 23:06:40 +0530

    Removed Duplicate code

--- spring-cloud-microservices-using-vault/product-service/src/main/java/com/programming/techie/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.programming.techie.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.programming.techie.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
